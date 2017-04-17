45722bd781f86900d29c68d234f8fbafe3ee8994
mudassir0909 <mudassir.dev@gmail.com>
2017-04-17 13:17:42 +1000

    Adds lights, shadows & basic animation
    * We add a light source using Three.SpotLight
    * Since MeshBasicMaterial does not react to light sources we change all the materials of objects to MeshLamberMaterial
    * Casting shadows is computationally intensive so we explicitly enable them by using castShadow/receiveShadow properties & renderer.shadowMapEnabled
    * Adds some basic animation using requestAnimationFrame

--- basic_static_scene/js/app.js
+++ basic_static_scene/js/app.js
@@ -1,52 +1,55 @@
 window.onload = init;
 
 function init() {
-  var scene = new THREE.Scene(); //container for all objects & lights that we want to render
-  // an object that defines, what we'll see when we render the scene
+  var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  //does the math of how the scene will look like based on camera's angle
-  var renderer = new THREE.WebGLRenderer(); //WebGLRenderer => use graphics card to render
+  var renderer = new THREE.WebGLRenderer();
   renderer.setClearColorHex(0xEEEEEE);
-  renderer.setClearColor(new THREE.Color(0xEEEEEE)); //setting background color of the scene
-  renderer.setSize(window.innerWidth, window.innerHeight); //fit entire available width & height
+  renderer.setClearColor(new THREE.Color(0xEEEEEE));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
 
-  // adds helper axes
   var axes = new THREE.AxisHelper(20);
   scene.add(axes);
 
-  // hepler plane
   var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1); //width: 60, height: 20
-  var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-  plane.rotation.x = -0.5 * Math.PI; //rotate 90 degress around X-axis
-  //👇 in the second chapter
+  plane.rotation.x = -0.5 * Math.PI;
   plane.position.x = 15;
   plane.position.y = 0;
   plane.position.z = 0;
+  plane.receiveShadow = true;
 
   scene.add(plane);
 
   var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
   var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
   cube.position.x = -4;
   cube.position.y = 3;
   cube.position.z = 0;
+  cube.castShadow = true;
 
   scene.add(cube);
 
   var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-  var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff, wireframe: true });
+  var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
   var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
   sphere.position.x = 20;
   sphere.position.y = 4;
   sphere.position.z = 2;
-
+  sphere.castShadow = true;
   scene.add(sphere);
+
+  var spotlight = new THREE.SpotLight(0xffffff);
+  spotlight.position.set(-40, 60, -10);
+  spotlight.castShadow = true;
+  scene.add(spotlight);
 
   camera.position.x = -30;
   camera.position.y = 40;
@@ -55,5 +58,22 @@ function init() {
 
   document.getElementById('webgl-output').appendChild(renderer.domElement);
 
-  renderer.render(scene, camera);
+  renderScene();
+
+  var step = 0;
+
+  function renderScene() {
+    //animating cube rotation
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
+    cube.rotation.z += 0.02;
+
+    //bouncing the sphere
+    step += 0.04;
+    sphere.position.x = 20 + (10 * Math.cos(step));
+    sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+    requestAnimationFrame(renderScene);
+    renderer.render(scene, camera);
+  }
 }
