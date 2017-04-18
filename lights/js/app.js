328028154233ad14a6932c65a50840e433a3a11f
mudassir0909 <mudassir.dev@gmail.com>
2017-04-18 22:59:38 +1000

    Init lights
    We start off with ambient light. An ambient light is the kind of light that illuminates each & every pixel equally & it doesn't cast shadow. An ambient in itself is not enough for a scene & hence we also included a spotlight in this example so that we could see the sphere & cube.
    
    A spotlight on the other hand, illuminates things in a cone shaped direction, more like a desk lamp. It does cast shadows.
    
    Why do we need ambient light then? An usecase would probably be rendering a scene which is supposed to portray hot temperature or cold temperature, we could use a orangish-red colored ambient light for a sunny day & blue colored ambient light for a winter-y day.

---
+++ lights/js/app.js
@@ -0,0 +1,101 @@
+window.onload = init;
+
+function init() {
+  var stats = initStats();
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setClearColorHex(0xEEEEEE);
+  renderer.setClearColor(new THREE.Color(0xEEEEEE));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
+
+  var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1); //width: 60, height: 20
+  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.x = 15;
+  plane.position.y = 0;
+  plane.position.z = 0;
+  plane.receiveShadow = true;
+
+  scene.add(plane);
+
+  var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  cube.position.x = -4;
+  cube.position.y = 3;
+  cube.position.z = 0;
+  cube.castShadow = true;
+
+  scene.add(cube);
+
+  var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+  var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+  sphere.position.x = 20;
+  sphere.position.y = 4;
+  sphere.position.z = 2;
+  sphere.castShadow = true;
+  scene.add(sphere);
+
+  var ambientColor = '#0c0c0c';
+  var ambientLight = new THREE.AmbientLight(ambientColor);
+  scene.add(ambientLight);
+
+  var controls = new function() {
+    this.ambientColor = ambientColor;
+  };
+  var gui = new dat.GUI();
+  gui.addColor(controls, 'ambientColor').onChange(function(e) {
+    ambientLight.color = new THREE.Color(e);
+  });
+
+  var spotlight = new THREE.SpotLight(0xffffff);
+  spotlight.position.set(-40, 60, -10);
+  spotlight.castShadow = true;
+  scene.add(spotlight);
+
+  camera.position.x = -30;
+  camera.position.y = 40;
+  camera.position.z = 30;
+  camera.lookAt(scene.position);
+
+  document.getElementById('webgl-output').appendChild(renderer.domElement);
+
+  renderScene();
+
+  window.addEventListener('resize', onResize, false);
+
+  var step = 0;
+
+  function renderScene() {
+    stats.update();
+
+    requestAnimationFrame(renderScene);
+    renderer.render(scene, camera);
+  }
+
+  function initStats() {
+    var stats = new Stats();
+
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.getElementById('stats-output').appendChild(stats.domElement);
+
+    return stats;
+  }
+
+  function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+}
