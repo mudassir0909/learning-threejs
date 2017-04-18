d5b8c1c6d28bb044173a5a2134f5293d16ea502b
mudassir0909 <mudassir.dev@gmail.com>
2017-04-18 23:28:42 +1000

    Add pointlight

--- lights/js/app.js
+++ lights/js/app.js
@@ -6,10 +6,9 @@ function init() {
   var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
   var renderer = new THREE.WebGLRenderer();
-  renderer.setClearColorHex(0xEEEEEE);
-  renderer.setClearColor(new THREE.Color(0xEEEEEE));
+  renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.shadowMapEnabled = true;
+  // renderer.shadowMapEnabled = true;
 
   var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1); //width: 60, height: 20
   var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
@@ -24,7 +23,7 @@ function init() {
   scene.add(plane);
 
   var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff7777 });
   var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
   cube.position.x = -4;
@@ -44,27 +43,50 @@ function init() {
   sphere.castShadow = true;
   scene.add(sphere);
 
+  camera.position.x = -25;
+  camera.position.y = 30;
+  camera.position.z = 25;
+  camera.lookAt(new THREE.Vector3(10, 0, 0));
+
   var ambientColor = '#0c0c0c';
   var ambientLight = new THREE.AmbientLight(ambientColor);
   scene.add(ambientLight);
 
+  var pointColor = '#ccffcc';
+  var pointLight = new THREE.PointLight(pointColor);
+  pointLight.distance = 100;
+  scene.add(pointLight);
+
+  // add a small sphere simulating the pointlight
+  var sphereLight = new THREE.SphereGeometry(0.2);
+  var sphereLightMaterial = new THREE.MeshBasicMaterial({color: 0xac6c25});
+  var sphereLightMesh = new THREE.Mesh(sphereLight, sphereLightMaterial);
+  sphereLightMesh.castShadow = true;
+
+  sphereLightMesh.position = new THREE.Vector3(3, 0, 3);
+  scene.add(sphereLightMesh);
+
   var controls = new function() {
+    this.rotationSpeed = 0.03;
+    this.bouncingSpeed = 0.03;
     this.ambientColor = ambientColor;
+    this.pointColor = pointColor;
+    this.intensity = 1;
+    this.distance = 100;
   };
   var gui = new dat.GUI();
   gui.addColor(controls, 'ambientColor').onChange(function(e) {
     ambientLight.color = new THREE.Color(e);
   });
-
-  var spotlight = new THREE.SpotLight(0xffffff);
-  spotlight.position.set(-40, 60, -10);
-  spotlight.castShadow = true;
-  scene.add(spotlight);
-
-  camera.position.x = -30;
-  camera.position.y = 40;
-  camera.position.z = 30;
-  camera.lookAt(scene.position);
+  gui.addColor(controls, 'pointColor').onChange(function (e) {
+    pointLight.color = new THREE.Color(e);
+  });
+  gui.add(controls, 'intensity', 0, 3).onChange(function(e) {
+    pointLight.intensity = e;
+  });
+  gui.add(controls, 'distance', 0, 100).onChange(function(e) {
+    pointLight.distance = e;
+  });
 
   document.getElementById('webgl-output').appendChild(renderer.domElement);
 
@@ -74,8 +96,39 @@ function init() {
 
   var step = 0;
 
+  var invert = 1;
+  var phase = 0;
+
   function renderScene() {
     stats.update();
+    // rotate the cube around its axes
+    cube.rotation.x += controls.rotationSpeed;
+    cube.rotation.y += controls.rotationSpeed;
+    cube.rotation.z += controls.rotationSpeed;
+
+    // bounce the sphere up and down
+    step += controls.bouncingSpeed;
+    sphere.position.x = 20 + ( 10 * (Math.cos(step)));
+    sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(step)));
+
+    // move the light simulation
+    if (phase > 2 * Math.PI) {
+        invert = invert * -1;
+        phase -= 2 * Math.PI;
+    } else {
+        phase += controls.rotationSpeed;
+    }
+    sphereLightMesh.position.z = +(7 * (Math.sin(phase)));
+    sphereLightMesh.position.x = +(14 * (Math.cos(phase)));
+    sphereLightMesh.position.y = 5;
+
+    if (invert < 0) {
+      var pivot = 14;
+      sphereLightMesh.position.x = (invert * (sphereLightMesh.position.x - pivot)) + pivot;
+    }
+
+    pointLight.position.copy(sphereLightMesh.position);
+    console.log(pointLight.position);
 
     requestAnimationFrame(renderScene);
     renderer.render(scene, camera);
